707f4aa1d65e7f33eaa6e8f07380953864244d49
David Rodriguez <davidrodrigueznolimit@gmail.com>
2024-08-05 14:11:20 -0500

    correct vercel redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        // Redirect HTTP to HTTPS
+        {
+          source: '/:path*',
+          has: [{ type: 'host', value: '(?!www\\.).*' }],
+          destination: 'https://www.:host/:path*',
+          permanent: true,
+        },
+        // Redirect windrose.dev to www.windrose.dev
+        {
+          source: '/:path*',
+          has: [{ type: 'host', value: 'windrose.dev' }],
+          destination: 'https://www.windrose.dev/:path*',
+          permanent: true,
+        },
+        // Redirect windroseandco.com to www.windroseandco.com
+        {
+          source: '/:path*',
+          has: [{ type: 'host', value: 'windroseandco.com' }],
+          destination: 'https://www.windroseandco.com/:path*',
+          permanent: true,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
