d3cb8635e456e00a1313d219bb1903a0240df856
David Rodriguez <davidrodrigueznolimit@gmail.com>
2024-08-05 15:53:16 -0500

    update config to enable test deployments

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,14 @@
 const nextConfig = {
   async redirects() {
     return [
-      // Redirect HTTP to HTTPS, except for localhost
+      // Redirect HTTP to HTTPS, except for localhost and vercel subdomains
       {
         source: "/:path*",
         has: [
           { type: "host", value: "(?!www\\.).*" },
-          { type: "host", value: "(?!localhost|127\\.0\\.0\\.1).*" },
+          { type: "host", value: "(?!localhost|127\\.0\\.0\\.1|.*\\.vercel\\.app).*" },
         ],
-        destination: "https://www.:host/:path*",
+        destination: "https://:host/:path*",
         permanent: true,
       },
       // Redirect windrose.dev to www.windrose.dev
@@ -25,30 +25,9 @@ const nextConfig = {
         has: [{ type: "host", value: "windroseandco.com" }],
         destination: "https://www.windroseandco.com/:path*",
         permanent: true,
-      },
-      // Redirect windrose-nextjs-git-main-windrose.vercel.app
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "windrose-nextjs-git-main-windrose.vercel.app" }],
-        destination: "https://www.windrose-nextjs-git-main-windrose.vercel.app/:path*",
-        permanent: true,
-      },
-      // Redirect windrose-nextjs.vercel.app
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "windrose-nextjs.vercel.app" }],
-        destination: "https://www.windrose-nextjs.vercel.app/:path*",
-        permanent: true,
-      },
-      // Redirect vercel.live/link/windrose-nextjs-git-dev-windrose.vercel.app?via=deployment-domains-list-branch
-      {
-        source: "/link/windrose-nextjs-git-dev-windrose.vercel.app",
-        has: [{ type: "host", value: "vercel.live" }],
-        destination: "https://www.windrose-nextjs-git-dev-windrose.vercel.app",
-        permanent: true,
-      },
+      }
     ];
-  },
+  }
 };
 
 export default nextConfig;
